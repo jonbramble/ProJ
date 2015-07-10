8b08c96758b5dccc4679b07c6cd24d4c21cd8833
Jonathan Bramble <jbramble82@gmail.com>
2015-07-10 15:27:24 +0100

    fixed signs

--- test.R
+++ test.R
@@ -2,7 +2,7 @@ library(deSolve)
 
 ABCD_First_Order <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
-    dA <- k1*A
+    dA <- -k1*A
     dB <- k1*A-k2*B
     dC <- k2*B-k3*C
     dD <- k3*C
@@ -10,12 +10,8 @@ ABCD_First_Order <- function(t, state, parameters) {
   })
 }
 
-parameters <- c(k1 = 0.02, k2 = -0.01, k3 = -0.01)
+parameters <- c(k1 = 0.05, k2 = 0.01, k3 = 0.01)
 state <- c(A=1, B=0, C=0, D=0)
 times <- seq(0, 100, by = 0.01)
 out <- ode(y = state, times = times, func = ABCD_First_Order, parms = parameters)
 plot(out)
-
-dfout = data.frame(out)
-dfout$sum = dfout$A+dfout$B+dfout$C+dfout$D
-plot(dfout$time,dfout$sum,type="l")
